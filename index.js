fc906745a3182b8e073f69b0aa43b01ce53ade03
tommyZZM <452490446@qq.com>
2018-11-01 11:00:00 +0800

    misc: reindent source file, add test dependencies

--- index.js
+++ index.js
@@ -17,100 +17,100 @@ const regexGlobalAlia = /global\.[\w\$_]+/;
 const regexPathAlia = /^\.\/.+/;
 
 module.exports = function (b, alias) {
-    let aliasPackagesMappingById = {};
-    let aliasPackages = Object.keys(alias).reduce((final,key)=>{
-        let value = alias[key];
-        let isRelativePath = regexPathAlia.test(value);
-        let pkg = {
-            id: isRelativePath ? path.join(cwd, value) : md5(key,10),
-            source:value,
-            deps:{},
-            isRelativePath
-        }
-
-        final[key] = pkg;
-        aliasPackagesMappingById[pkg.id] = pkg;
-        return final
-    },{});
-
-    b._bresolve = function (id, parent, cb) {
-        if (alias[id]) {
-            return cb(null,_empty,{})
-        }
-        return bresolve(id,parent,cb)
+  let aliasPackagesMappingById = {};
+  let aliasPackages = Object.keys(alias).reduce((final, key) => {
+    let value = alias[key];
+    let isRelativePath = regexPathAlia.test(value);
+    let pkg = {
+      id: isRelativePath ? path.join(cwd, value) : md5(key, 10),
+      source: value,
+      deps: {},
+      isRelativePath
     }
 
-    resetPipeline(b);
-    b.on('reset',()=>resetPipeline(b))
+    final[key] = pkg;
+    aliasPackagesMappingById[pkg.id] = pkg;
+    return final
+  }, {});
 
-    function resetPipeline(b) {
-        b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
-            Object.keys(chunk.deps).forEach(key=>{
-                let pkg = aliasPackages[key];
-                if (pkg) {
-                    chunk.deps[key] = pkg.id
-                }
-            });
+  b._bresolve = function (id, parent, cb) {
+    if ( alias[id] ) {
+      return cb(null, _empty, {})
+    }
+    return bresolve(id, parent, cb)
+  }
 
-            if (aliasPackagesMappingById[chunk.id]) {
-                aliasPackagesMappingById[chunk.id].isDependencied = true;
+  resetPipeline(b);
+  b.on('reset', () => resetPipeline(b))
+
+  function resetPipeline(b) {
+    b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
+      Object.keys(chunk.deps).forEach(key => {
+        let pkg = aliasPackages[key];
+        if ( pkg ) {
+          chunk.deps[key] = pkg.id
+        }
+      });
+
+      if ( aliasPackagesMappingById[chunk.id] ) {
+        aliasPackagesMappingById[chunk.id].isDependencied = true;
+      }
+
+      return (chunk.id === _empty) ? next() : next(null, chunk)
+    }, function (flush) {
+      Promise.all(Object.keys(aliasPackages).map(key => new Promise((done) => {
+        let pkg = aliasPackages[key];
+        let source = new Buffer("");
+
+        transformSource(pkg.id, pkg.source)
+          .pipe(b._mdeps.getTransforms(pkg.id, {}, { builtin: true }))
+          .pipe(through(function (buf, _, next) {
+            source = Buffer.concat([source, buf])
+            next(null, buf)
+          }, function (flush) {
+            let { id, deps, isRelativePath } = pkg;
+
+            let file = path.join(__dirname, "./lib/_empty");
+            if ( isRelativePath ) {
+              file = pkg.id;
             }
 
-            return (chunk.id === _empty) ? next() : next(null,chunk)
-        },function (flush) {
-            Promise.all(Object.keys(aliasPackages).map(key=>new Promise((done)=>{
-                let pkg = aliasPackages[key];
-                let source = new Buffer("");
-
-                transformSource(pkg.id, pkg.source)
-                    .pipe(b._mdeps.getTransforms(pkg.id,{},{builtin:true}))
-                    .pipe(through(function (buf,_,next) {
-                        source = Buffer.concat([source,buf])
-                        next(null,buf)
-                    },function (flush) {
-                        let { id, deps, isRelativePath } = pkg;
-
-                        let file = path.join(__dirname, "./lib/_empty");
-                        if (isRelativePath) {
-                            file = pkg.id;
-                        }
-
-                        done({
-                            ...pkg
-                            , id
-                            , deps
-                            , source:source.toString()
-                            , file
-                        })
-                        
-                        flush()
-                    }))
-            }))).then((alias)=>{
-                alias.forEach(pkg=>{
-                    if (!pkg.isDependencied) this.push(pkg);
-                })
-                flush()
+            done({
+              ...pkg
+              , id
+              , deps
+              , source: source.toString()
+              , file
             })
-        }))
 
-    }
+            flush()
+          }))
+      }))).then((alias) => {
+        alias.forEach(pkg => {
+          if ( !pkg.isDependencied ) this.push(pkg);
+        })
+        flush()
+      })
+    }))
+
+  }
 }
 
 function transformSource(id, sourcePath) {
-    if (regexGlobalAlia.test(sourcePath)) {
-        return stringStream("module.exports = "+ sourcePath);
-    }
+  if ( regexGlobalAlia.test(sourcePath) ) {
+    return stringStream("module.exports = " + sourcePath);
+  }
 
-    if (path.isAbsolute(id)) {
-        return fs.createReadStream(id);
-    }
+  if ( path.isAbsolute(id) ) {
+    return fs.createReadStream(id);
+  }
 
-    return stringStream("module.exports = void 0")
+  return stringStream("module.exports = void 0")
 
-    function stringStream(input) {
-        let st = through();
-        st.push(input);
-        st.push(null);
-        return st
-    }
+  function stringStream(input) {
+    let st = through();
+    st.push(input);
+    st.push(null);
+    return st
+  }
 }
--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@
 "use strict";
 const path = require("path");
 const browserify = require("browserify");
+const babelify = require("babelify");
 const through = require("through2");
 const mocha = require("mocha");
 const chai = require("chai");
@@ -15,106 +16,106 @@ chai.use(spies);
 const describe = mocha.describe;
 const alias = require("../");
 
-describe('test', function() {
-    it("alias global.*",function(done){
-        let b = browserify({entries:path.resolve(__dirname,"./src/index-global.js")})
-            .plugin(alias,{
-                vue:"global.Vue"
-            })
+describe('test', function () {
+  it("alias global.*", function (done) {
+    let b = browserify({ entries: path.resolve(__dirname, "./src/index-global.js") })
+      .plugin(alias, {
+        vue: "global.Vue"
+      })
 
-        let check = (curr) => next => expect(curr).to.be.equal(next)
-        b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
-            next(null,chunk)
-            if (chunk.entry) {
-                check = check(chunk.deps["vue"])
-            } else {
-                check(chunk.id)
-            }
-        }))
+    let check = (curr) => next => expect(curr).to.be.equal(next)
+    b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
+      next(null, chunk)
+      if ( chunk.entry ) {
+        check = check(chunk.deps["vue"])
+      } else {
+        check(chunk.id)
+      }
+    }))
 
-        return b.bundle().on("end",_=>done()).pipe(through())
+    return b.bundle().on("end", _ => done()).pipe(through())
+  })
+
+  it("alias global.* with debug=true", function (done) {
+    let b = browserify({
+      entries: path.resolve(__dirname, "./src/index-global.js"),
+      debug: true
     })
+      .plugin(alias, {
+        vue: "global.Vue"
+      })
 
-    it("alias global.* with debug=true",function(done){
-        let b = browserify({
-                entries:path.resolve(__dirname,"./src/index-global.js"),
-                debug:true
-            })
-            .plugin(alias,{
-                vue:"global.Vue"
-            })
+    let check = (curr) => next => expect(curr).to.be.equal(next)
+    b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
+      next(null, chunk)
+      if ( chunk.entry ) {
+        check = check(chunk.deps["vue"])
+      } else {
+        check(chunk.id)
+      }
+    }))
 
-        let check = (curr) => next => expect(curr).to.be.equal(next)
-        b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
-            next(null,chunk)
-            if (chunk.entry) {
-                check = check(chunk.deps["vue"])
-            } else {
-                check(chunk.id)
-            }
-        }))
+    return b.bundle().on("end", _ => done()).pipe(through())
+  })
 
-        return b.bundle().on("end",_=>done()).pipe(through())
-    })
+  it("should be reject", function (done) {
+    let b = browserify({ entries: path.resolve(__dirname, "./src/reject.js") })
+      .plugin(alias, {
+        vue: "global.Vue"
+      })
+    let onerror = chai.spy(err => {
+      //console.log(err.message);
+    });
 
-    it("should be reject",function(done){
-        let b = browserify({entries:path.resolve(__dirname,"./src/reject.js")})
-                .plugin(alias,{
-                    vue:"global.Vue"
-                })
-        let onerror = chai.spy(err=>{
-            //console.log(err.message);
-        });
+    b.bundle().on("error", (err) => {
+      onerror(err);
+      expect(onerror).to.have.been.called();
+      done();
+    }).on("end", _ => {
+      done()
+    }).pipe(through());
 
-        b.bundle().on("error",(err)=>{
-            onerror(err);
-            expect(onerror).to.have.been.called();
-            done();
-        }).on("end",_=>{
-            done()
-        }).pipe(through());
+  })
 
-    })
+  it("alias to a redirected file (standalone)", function (done) {
+    let b = browserify({ entries: path.resolve(__dirname, "./src/index-redirect-standalone.js") })
+      .plugin(alias, {
+        vue: "global.Vue",
+        redirect: "./test/src/fixture-redirect.js"
+      })
 
-    it("alias to a redirected file (standalone)",function(done){
-        let b = browserify({entries:path.resolve(__dirname,"./src/index-redirect-standalone.js")})
-            .plugin(alias,{
-                vue:"global.Vue",
-                redirect:"./test/src/fixture-redirect.js"
-            })
+    const allChunkIds = []
+    b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
+      if ( chunk.entry ) {
+        const keys = Object.keys(chunk.deps);
+      }
+      allChunkIds.push(chunk.id);
+      next(null, chunk);
+    }))
 
-        const allChunkIds = []
-        b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
-            if (chunk.entry) {
-                const keys = Object.keys(chunk.deps);
-            }
-            allChunkIds.push(chunk.id);
-            next(null,chunk);
-        }))
+    return b.bundle().on("end", _ => {
+      expect(allChunkIds.length).to.be.equal(3);
+      expect(allChunkIds).to.include(path.join(cwd, "./test/src/fixture-redirect.js"));
+      done();
+    }).pipe(through());
+  })
 
-        return b.bundle().on("end",_=>{
-            expect(allChunkIds.length).to.be.equal(3);
-            expect(allChunkIds).to.include(path.join(cwd, "./test/src/fixture-redirect.js"));
-            done();
-        }).pipe(through());
-    })
+  it("alias to a redirected file (multiply)", function (done) {
+    let b = browserify({ entries: path.resolve(__dirname, "./src/index-redirect-multiply.js") })
+      .plugin(alias, {
+        vue: "global.Vue",
+        redirect: "./test/src/fixture-redirect.js"
+      })
 
-    it("alias to a redirected file (multiply)",function(done){
-        let b = browserify({entries:path.resolve(__dirname,"./src/index-redirect-multiply.js")})
-            .plugin(alias,{
-                vue:"global.Vue",
-                redirect:"./test/src/fixture-redirect.js"
-            })
+    b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
+      next(null, chunk);
+      if ( chunk.entry ) {
+        const keys = Object.keys(chunk.deps);
+        expect(keys.length).to.be.equal(2);
+        expect(chunk.deps[keys[0]]).to.be.equal(chunk.deps[keys[1]]);
+      }
+    }))
 
-        b.pipeline.get("deps").push(through.obj(function (chunk, enc, next) {
-            next(null,chunk);
-            if (chunk.entry) {
-                const keys = Object.keys(chunk.deps);
-                expect(keys.length).to.be.equal(2);
-                expect(chunk.deps[keys[0]]).to.be.equal(chunk.deps[keys[1]]);
-            }
-        }))
-
-        return b.bundle().on("end",_=>done()).pipe(through())
-    })
+    return b.bundle().on("end", _ => done()).pipe(through())
+  })
 })
